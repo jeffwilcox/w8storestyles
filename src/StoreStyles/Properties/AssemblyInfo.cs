b61ac24fdde563f04773c18bfae7a3c30f63b9a9
Jeff Wilcox <jeffwilcox@gmail.com>
2012-09-17 16:01:01 -0700

    initial commit

---
+++ src/StoreStyles/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StoreStyles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StoreStyles")]
+[assembly: AssemblyCopyright("Copyright ©  Jeff Wilcox")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
